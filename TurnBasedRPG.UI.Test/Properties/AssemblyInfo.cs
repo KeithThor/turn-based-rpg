54c6abc8bf8ce83648b7f0aa63a613aa08f8f7d3
Keith Thor <keithkeethor@gmail.com>
2018-10-15 15:32:00 -0700

    Add partial unit test for UI assembly
    Added unit testing for the action panel and combat formation UIs. In
    the process, found and fixed some minor bugs. Unit testing might not be
    a great idea for highly malleable code such as the UI which might
    undergo several breaking changes which might each require changes to
    the unit testing code as well.

---
+++ TurnBasedRPG.UI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TurnBasedRPG.UI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TurnBasedRPG.UI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f74faf96-775f-4c6d-ae2f-bda788105bae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
